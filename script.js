f202f5e21ddad11a9526ebc771d72fec05c0bf27
klar-sites <klar.sites@twixly.io>
2026-06-29 22:17:35 +0200

    Update content via Klar visual CMS

--- script.js
+++ script.js
@@ -136,5 +136,5 @@ function setActive() {
 // Mobile menu button
 document.querySelector('.vkn-burger').onclick = function(e) {
   console.log(e.target)
-  e.target.nextSibling.nextSibling.style.display = 'block';
+  e.target.nextSibling.style.display = 'block';
 }
